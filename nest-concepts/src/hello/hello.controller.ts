c24b2dc1607f3a4dfe518790701b317944380b46
Gazi2050 <gazi45344@gmail.com>
2025-06-02 13:17:04 +0600

    feat:add /hello route

--- nest-concepts/src/hello/hello.controller.ts
+++ nest-concepts/src/hello/hello.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { HelloService } from './hello.service';
 
+//incoming request and returning responses
+// get , post,put,delete
+//localhost:3000/hello
 @Controller('hello')
-export class HelloController {}
+export class HelloController {
+    //dependency injection
+    constructor(private readonly helloService: HelloService) { }
+
+    @Get()
+    getHello(): string {
+        return this.helloService.getHello()
+    }
+}
