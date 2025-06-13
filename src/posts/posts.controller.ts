7a294d20e188b267e84b137c8e149be46a3a29da
Gazi2050 <gazi45344@gmail.com>
2025-06-13 21:40:55 +0600

    feat:add getAllPosts api

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+    constructor(private postService: PostsService) { }
+
+    @Get()
+    async getAllPosts() {
+        const posts = await this.postService.findAll();
+
+        if (posts.length === 0) {
+            return { message: 'No posts found' };
+        }
+        return posts;
+    }
+}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+    constructor(private prisma: PrismaService) { }
+
+    async findAll() {
+        return this.prisma.post.findMany({
+            include: { author: true },
+        });
+    }
+
+}
